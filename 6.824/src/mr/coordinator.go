bcfb447c74882851fe5f9d8ce321e0ebe8e2d0c9
huzhiyang <164177565@qq.com>
2022-07-18 22:20:57 +0800

    6.824

---
+++ 6.824/src/mr/coordinator.go
@@ -0,0 +1,153 @@
+package mr
+
+import (
+	"log"
+	"sync"
+)
+import "net"
+import "os"
+import "net/rpc"
+import "net/http"
+
+const (
+	Idle      = 1
+	InProcess = 2
+	Complete  = 3
+)
+
+const (
+	MapJob    = 1
+	ReduceJob = 2
+)
+
+// Job map or reduce job
+type Job struct {
+	JobType int
+	File    []string
+	Id      int64
+	State   int
+	nReduce int
+}
+
+type Coordinator struct {
+	// Your definitions here.
+	// master 节点
+	Idx      int
+	IsFinish bool
+	
+	jobTask   chan Job
+	reduceNum int
+	nReduce   int
+	
+	wg sync.WaitGroup
+}
+
+// Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) GenMapTask() {
+	
+}
+
+func (c *Coordinator) Task(arg *Arg, reply *Reply) error {
+	job := <-c.jobTask
+	reply.Job = job
+	
+	go func() {
+		job.State = InProcess
+		c.jobTask <- job
+	}()
+	// todo timeout check
+	
+	return nil
+}
+
+func (c *Coordinator) Finish(arg FinishArg, reply FinishReply) error {
+	switch arg.JobType {
+	case MapJob:
+		if len(c.jobTask) == 0 {
+			// generate reduce task
+			
+		}
+	
+	case ReduceJob:
+	
+	}
+	
+	return nil
+}
+
+//
+// an example RPC handler.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
+}
+
+//
+// start a thread that listens for RPCs from worker.go
+//
+func (c *Coordinator) server() {
+	rpc.Register(c)
+	rpc.HandleHTTP()
+	// l, e := net.Listen("tcp", ":1234")
+	sockname := coordinatorSock()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
+
+// Done
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+func (c *Coordinator) Done() bool {
+	// ret := false
+	
+	// Your code here.
+	
+	return c.IsFinish
+}
+
+// MakeCoordinator create a Coordinator.
+// main/mrcoordinator.go calls this function.
+// nReduce is the number of reduce tasks to use.
+func MakeCoordinator(files []string, nReduce int) *Coordinator {
+	// Your code here.
+	
+	c := Coordinator{
+		nReduce: nReduce,
+	}
+	
+	// generate task queue
+	taskList := make([]Job, len(files))
+	
+	for i, file := range files {
+		task := taskList[i]
+		
+		task.Id = int64(i)
+		task.JobType = MapJob
+		task.State = Idle
+		task.File = append(task.File, file)
+		task.nReduce = nReduce
+		
+		taskList[i] = task
+	}
+	
+	log.Printf("%v", taskList)
+	
+	c.jobTask = make(chan Job, nReduce)
+	for _, task := range taskList {
+		c.jobTask <- task
+	}
+	
+	c.server()
+	return &c
+}
+
+func (c *Coordinator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	
+}
---
+++ 6.824/src/mr/worker.go
@@ -0,0 +1,205 @@
+package mr
+
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+)
+
+type Work struct {
+	Idx   int
+	State int
+}
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+// d
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+type workArg struct {
+}
+
+type workReply struct {
+	Id      int64
+	File    string
+	Content string
+}
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+	
+	//
+	// a big difference from real MapReduce is that all the
+	// intermediate data is in one place, intermediate[],
+	// rather than being partitioned into NxM buckets.
+	//
+	
+	// Your worker implementation here.
+	var reply Reply
+	
+	call("Coordinator.Task", &Arg{}, &reply)
+	
+	log.Printf("%v", reply)
+	
+	switch reply.JobType {
+	case MapJob:
+		log.Printf("task is map job")
+		intermediate := make([]KeyValue, 0)
+		for _, filename := range reply.File {
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatalf("cannot open %v", filename)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			file.Close()
+			kva := mapf(filename, string(content))
+			intermediate = append(intermediate, kva...)
+		}
+		sort.Sort(ByKey(intermediate))
+		
+		nReduce := reply.nReduce
+		
+		outPutFiles := make([]*os.File, reply.nReduce)
+		fileEncs := make([]*json.Encoder, nReduce)
+		
+		for i := 0; i < nReduce; i++ {
+			outPutFiles[i], _ = os.CreateTemp("mr-tmp", fmt.Sprintf("mr-%d-%d", reply.Id, i))
+			fileEncs[i] = json.NewEncoder(outPutFiles[i])
+		}
+		
+		for _, kv := range intermediate {
+			i := ihash(kv.Key) % reply.nReduce
+			enc := fileEncs[i]
+			
+			err := enc.Encode(&kv)
+			if err != nil {
+				panic("json encode failed")
+			}
+		}
+	
+	case ReduceJob:
+		log.Printf("task is reduce job")
+		
+		reducef("1", reply.File)
+	}
+	
+	Finish(reply.Job)
+	
+	// uncomment to send the Example RPC to the coordinator.
+	// var reply mapTaskReply
+	// call("Coordinator.Map", &mapTaskArg{}, &reply)
+	
+	// keyValues := mapf(reply.File, reply.)
+	
+}
+
+// func WriteMapToFile() {
+//
+// }
+//
+// func WriteReduceToFile() {
+//
+// 	//
+// 	// a big difference from real MapReduce is that all the
+// 	// intermediate data is in one place, intermediate[],
+// 	// rather than being partitioned into NxM buckets.
+// 	//
+//
+// 	sort.Sort(ByKey(intermediate))
+//
+// 	oname := "mr-out-0"
+// 	ofile, _ := os.Create(oname)
+// }
+
+//
+// example function to show how to make an RPC call to the coordinator.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func CallExample() {
+	
+	// declare an argument structure.
+	args := ExampleArgs{}
+	
+	// fill in the argument(s).
+	args.X = 99
+	
+	// declare a reply structure.
+	reply := ExampleReply{}
+	
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.Example", &args, &reply)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("reply.Y %v\n", reply.Y)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+}
+
+func Finish(job Job) {
+	args := FinishArg{Job: job}
+	reply := FinishReply{}
+	ok := call("Coordinator.Example", &args, &reply)
+	if ok {
+		fmt.Printf("finished")
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+}
+
+//
+// send an RPC request to the coordinator, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+	
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+	
+	fmt.Println(err)
+	return false
+}
